b556548384cdcace46c99ce8979b39b0b480a95d
dcaamal <47618582+dcaamal@users.noreply.github.com>
2020-05-27 17:50:42 -0500

    Update theta_optim.R

--- R/theta_optim.R
+++ R/theta_optim.R
@@ -1,10 +1,12 @@
 #Function that selects the bandwidth parameter of a Gaussian kernel
 #K(i,j)=exp(-\theta (d_ij)^2))
 
-theta_optim<-function(y,X)
+theta_optim<-function(y, X, D = NULL, b = NULL)
 {
   #y: response vector (n x 1)
   #X: matrix of markers (n x p)
+  #D: distance matrix (p x p)
+  #b: grid
   
   #check inputs
   if (is.null(rownames(X))) stop("X must have rownames\n")
@@ -14,23 +16,30 @@ theta_optim<-function(y,X)
   n <- nrow(X) #number of individuals
   p <- ncol(X) #number of markers
   
-  #standardize the matrix of markers
-  Z <- scale(X,center=TRUE,scale=TRUE) 
-  #euclidian distance matrix
-  D <- as.matrix(dist(Z, method = "euclidian"))/sqrt(p)
-  #vector of values of theta
-  theta <- setdiff(seq(0, max(D), length.out = 11),0)
+ #check the distance matrix
+  if (is.null(D)) {
+    #standardize the matrix of markers
+    Z <- scale(X,center=TRUE,scale=TRUE) 
+    #euclidian distance matrix
+    D <- as.matrix(dist(Z, method = "euclidian"))/sqrt(p)
+  }
+  
+  #grid of valures of theta
+  if (is.null(b)){
+    #vector of values of theta
+    b <- setdiff(seq(0, max(D), length.out = 11),0)
+  }
   
   #Step 2:
   sol <- list()
   out <- list()
-  nt  <- length(theta)
+  nt  <- length(b)
   
   #fit the model for each theta value
   for (i in 1:nt)
   {
     cat("i=",i,"\n")
-    KG <- exp(-theta[i]*(D^2))	
+    KG <- exp(-b[i]*(D^2))	
     random <- list(kernel=list(K=KG, id=rownames(X)))
     sol[[i]] <- lmer_uvcov(y=y,fixed="1",random=random) 
   }
@@ -46,9 +55,9 @@ theta_optim<-function(y,X)
   
   LL <- as.numeric(LL)
   max.LL <- which.max(LL)
-  theta.max <- theta[max.LL]
-  K <- exp(-theta[max.LL]*(D^2))
+  theta.max <- b[max.LL]
+  K <- exp(-b[max.LL]*(D^2))
   
-  return(list(LL=LL, max.LL=LL[max.LL], theta=theta, 
+  return(list(LL=LL, max.LL=LL[max.LL], theta=b, 
               theta.max=theta.max, K=K))
 }
