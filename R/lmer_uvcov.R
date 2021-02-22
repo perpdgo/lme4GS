98b9ad9af4805f3461221b54a7c3f7cb11cd998b
dcaamal <47618582+dcaamal@users.noreply.github.com>
2021-02-21 20:16:28 -0600

    Add files via upload

---
+++ R/lmer_uvcov.R
@@ -0,0 +1,124 @@
+#Function to fit a mixed model:
+#y=X*beta + Z_1 u_1 + Z_2 u_2 + ... + Z_k u_k + e 
+#NA are not allowed in y
+
+lmer_uvcov<-function(y,fixed="1",random,verbose=5L)
+{
+	
+	#Check inputs
+	if(class(random)!="list") stop("random should be a list\n")
+	if(is.null(names(random)) | any(nchar(names(random))==0)) stop("Each element of the list 'random' must be named\n")
+
+	#Check the factoring method and assign 'auto' if it was not assigned
+	for(k in 1:length(random))
+	{
+		if(is.null(random[[k]]$f_method))
+		{
+			random[[k]]$f_method<-"auto"
+			message("Factoring method for vcov matrix set to 'auto' for ",names(random)[k])
+		}else{
+			if(!(random[[k]]$f_method%in%c("auto","chol","evd"))) stop("Supported factoring methods are: 'auto', 'chol' or 'evd'.\n")
+		}
+	}
+	
+	#Step 1 parse the formula
+	control<-lmerControl()
+	control$checkControl$check.nobs.vs.rankZ <- "ignore"
+	control$checkControl$check.nobs.vs.nlev <- "ignore"
+	control$checkControl$check.nobs.vs.nRE <- "ignore"
+	
+	#Get the random effects' names and extract their Id 
+	list_tmp = list()
+	for (name in names(random)) {
+		list_tmp[[name]] = random[[name]][["id"]]
+	}
+
+	#Create the dataframe to be passed into lFormula
+	#Avoid transforming random effects Ids to factors.
+	data_df = data.frame(matrix(unlist(list_tmp), nrow = max(lengths(list_tmp))), stringsAsFactors = FALSE)
+
+	colnames(data_df) <- names(random)
+	#Keep variable y as numeric
+
+	data_df$y = y
+	#rownames(random_df) <- colnames(random[[1]][["K"]])
+
+	#Create formula
+	f <-"y~"
+	f<-paste0(f,fixed)
+
+	#Add random effects' names to the formula. With the previous modification it is not necessary to use the dataframe name in
+	#the formula
+	for(k in names(random)){
+	    if(k != "y"){
+			f<-paste0(f,"+ (1|", k ,")")
+		}
+	}
+	cat(f)
+	
+	#We must use the data parameter and adjust the f formula (as we did previously) in order to avoid issues as explained here
+	#https://rdrr.io/cran/lme4/man/modular.html
+  
+	parsedFormula<-lFormula(f,control=control, data=data_df)
+	
+	#At this point random_df is useless, We only needed it to create parsedFormula. 
+	#We will keep using the original random variable afterwards.
+	
+	#Step 2, deviance function
+	
+	for(k in 1:length(random))
+	{
+	    #If K is NULL then we assign the Identity matrix
+	    if(!is.null(random[[k]]$K))
+	    {
+			random[[k]]$id<-as.factor(random[[k]]$id)
+			ids.unique<-levels(random[[k]]$id)
+			random[[k]]$K<-Matrix::Matrix(random[[k]]$K[ids.unique,ids.unique],sparse=TRUE)
+	    }
+	}
+	
+	#SOME THING VERY STRANGE HERE, IT IS POSSIBLE THAT THE ORDER OF ELEMENTS IN Ztlist 
+	#ARE NOT THE SAME THAT IN THE LIST random
+	
+	Ztlist<-parsedFormula$reTrms[["Ztlist"]]
+	fnmns<-names(parsedFormula$reTrms[["flist"]])
+	
+	#The names as I was expecting
+	#expected_names<-paste0("random[[",1:length(random),"]]$id")
+	expected_names <- names(random)
+	
+	for(k in 1:length(random))
+	{
+		#In which position of the list fnmns is random[[k]]$id
+		j<-which(fnmns[k]==expected_names)
+		
+		#If K is NULL then we assign the Identity matrix
+		if(!is.null(random[[j]]$K))
+		{
+			Ztlist[[k]]<-relfac(random[[j]]$K,random[[j]]$f_method,1e-10)%*%Ztlist[[k]]
+			
+			#Ztlist[[k]]<-relfac(random[[j]]$K)%*%Ztlist[[k]]
+		}
+	}
+	
+	parsedFormula$reTrms[["Ztlist"]]<-Ztlist
+	parsedFormula$reTrms[["Zt"]]<-do.call(rbind, Ztlist)  
+	
+	devianceFunction<-do.call(mkLmerDevfun, c(parsedFormula,
+    					          list(start = NULL, verbose = TRUE, control = control)))
+	
+	#3 The optimization module
+	optimizerOutput<-optimizeLmer(devianceFunction,verbose=verbose)
+
+	#4 Output module 
+	out<-mkMerMod(rho = environment(devianceFunction), opt = optimizerOutput,
+                  reTrms = parsedFormula$reTrms, fr = parsedFormula$fr)
+                  
+    
+    #Add attributes
+    attr(out@frame,"enmns")<-expected_names   #Expected names
+    attr(out@frame,"rnmns")<-names(random)    #the name of the elements in the list 'random' provided by user
+    attr(out@frame,"fnmns")<-fnmns            #the name of the elements in the list created by lme4
+    
+    return(out)               
+}
---
+++ R/theta_optim.R
@@ -0,0 +1,63 @@
+#Function that selects the bandwidth parameter of a Gaussian kernel
+#K(i,j)=exp(-\theta (d_ij)^2))
+
+theta_optim<-function(y, X, D = NULL, b = NULL)
+{
+  #y: response vector (n x 1)
+  #X: matrix of markers (n x p)
+  #D: distance matrix (p x p)
+  #b: grid
+  
+  #check inputs
+  if (is.null(rownames(X))) stop("X must have rownames\n")
+  if (!is.character(rownames(X))) (rownames(X)<-as.character(rownames(X)))
+  
+  #Step 1:
+  n <- nrow(X) #number of individuals
+  p <- ncol(X) #number of markers
+  
+ #check the distance matrix
+  if (is.null(D)) {
+    #standardize the matrix of markers
+    Z <- scale(X,center=TRUE,scale=TRUE) 
+    #euclidian distance matrix
+    D <- as.matrix(dist(Z, method = "euclidian"))/sqrt(p)
+  }
+  
+  #grid of valures of theta
+  if (is.null(b)){
+    #vector of values of theta
+    b <- setdiff(seq(0, max(D), length.out = 11),0)
+  }
+  
+  #Step 2:
+  sol <- list()
+  out <- list()
+  nt  <- length(b)
+  
+  #fit the model for each theta value
+  for (i in 1:nt)
+  {
+    cat("i=",i,"\n")
+    KG <- exp(-b[i]*(D^2))	
+    random <- list(kernel=list(K=KG, id=rownames(X)))
+    sol[[i]] <- lmer_uvcov(y=y,fixed="1",random=random) 
+  }
+  
+  #Step 3:
+  LL <- rep(0, nt)
+  
+  for (i in 1:nt)
+  {
+    out[[i]] <- summary(sol[[i]])  
+    LL[i] <- out[[i]]$logLik
+  }
+  
+  LL <- as.numeric(LL)
+  max.LL <- which.max(LL)
+  theta.max <- b[max.LL]
+  K <- exp(-b[max.LL]*(D^2))
+  
+  return(list(LL=LL, max.LL=LL[max.LL], theta=b, 
+              theta.max=theta.max, K=K))
+}
