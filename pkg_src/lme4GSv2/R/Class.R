95fd5a4abb00811076be57bf5ca89cf73a1e9483
Paulino Pérez-Rodríguez <perpdgo@gmail.com>
2026-01-26 13:15:34 -0600

    Delete pkg_src/lme4GSv2/R directory

--- pkg_src/lme4GSv2/R/Class.R
+++
@@ -1,7 +0,0 @@
-#Classes for lmerUvcov and glmerUvcov
-
-setClass("lmerUvcov", representation = list(resp="lmerResp",relfac = "list"),
-          contains = "merMod")
-setClass("glmerUvcov", representation = list(resp = "glmResp", relfac = "list"),
-         contains = "merMod")
-
